c150ce49d18314156fe70628faf14beaca48e5bd
Wuuuu <moriafly@163.com>
2022-05-22 09:57:39 +0800

    添加 MarqueeText

--- lovecompose/src/main/java/com/moriafly/lovecompose/assist/Fragment.kt
+++ lovecompose/src/main/java/com/moriafly/lovecompose/assist/Fragment.kt
@@ -1,3 +1,21 @@
+@file:Suppress("unused", "UNUSED_PARAMETER")
+
+/*
+ * Copyright 2022 Moriafly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.moriafly.lovecompose.assist
 
 import android.widget.FrameLayout
@@ -7,6 +25,9 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 
+/**
+ * 在 Compose 中添加 Fragment
+ */
 @Composable
 fun Fragment(
     modifier: Modifier = Modifier,
---
+++ lovecompose/src/main/java/com/moriafly/lovecompose/assist/MarqueeText.kt
@@ -0,0 +1,89 @@
+@file:Suppress("unused", "UNUSED_PARAMETER")
+
+/*
+ * Copyright 2022 Moriafly
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.moriafly.lovecompose.assist
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.AttributeSet
+import android.util.TypedValue
+import androidx.annotation.Px
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.viewinterop.AndroidView
+
+/**
+ * Marquee Text
+ *
+ * @param text 文本
+ * @param modifier
+ * @param color 文本颜色
+ * @param fontSizePx 文本大小（单位：像素 px）
+ * @param isHorizontalFadingEdgeEnabled 两边是否淡出
+ * @param marqueeRepeatLimit 滚动次数
+ */
+@Composable
+fun MarqueeText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = Color.Unspecified,
+    @Px fontSizePx: Float,
+    isHorizontalFadingEdgeEnabled: Boolean = true,
+    marqueeRepeatLimit: Int = 1
+) {
+    AndroidView(
+        factory = { context ->
+            MarqueeTextView(context)
+        },
+        modifier = modifier,
+        update = { view ->
+            view.apply {
+                setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSizePx)
+                this.text = text
+                setTextColor(color.toArgb())
+                this.isHorizontalFadingEdgeEnabled = isHorizontalFadingEdgeEnabled
+                this.marqueeRepeatLimit = marqueeRepeatLimit
+            }
+        }
+    )
+}
+
+private class MarqueeTextView: AppCompatTextView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    override fun isFocused(): Boolean {
+        return true
+    }
+
+    init {
+        this.ellipsize = TextUtils.TruncateAt.MARQUEE
+        this.isSingleLine = true
+        this.marqueeRepeatLimit = 1
+        this.isFocusableInTouchMode = true
+        this.isHorizontalFadingEdgeEnabled = true
+    }
+}
